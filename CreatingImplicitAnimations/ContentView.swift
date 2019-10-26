e629356b3a409bddac01ddd76f28384e25d19ba4
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-26 18:58:57 +0300

    init

--- CreatingImplicitAnimations/ContentView.swift
+++ CreatingImplicitAnimations/ContentView.swift
@@ -9,8 +9,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount: CGFloat = 1
     var body: some View {
-        Text("Hello World")
+        Button("Tap Me") {
+            self.animationAmount += 1
+        }.padding(50)
+        .background(Color.red)
+        .foregroundColor(Color.white)
+        .clipShape(Circle())
+        .scaleEffect(animationAmount)
+        .blur(radius: (animationAmount - 1) * 3)
+        .animation(.default)
     }
 }
 
